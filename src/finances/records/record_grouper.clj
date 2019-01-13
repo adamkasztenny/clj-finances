9fbf64c3d8eb5031bd84c6b9a703647516ea9e33
Adam Kasztenny <adamkasztenny@gmail.com>
2019-01-13 18:08:23 -0500

    Make record grouper do a fuzzy text search in each record's description (as I originally intended but forgot to do)

--- src/finances/records/record_grouper.clj
+++ src/finances/records/record_grouper.clj
@@ -1,10 +1,10 @@
 (ns finances.records.record_grouper
+  (:require [clojure.string :as string])
   (:gen-class))
 
-;; contains? was clausing me problems...
-;; thanks to https://stackoverflow.com/questions/3249334/test-whether-a-list-contains-a-specific-value-in-clojure
-;; for clearing this up
-(defn- in? [coll x] (if (some #{x} coll) true false))
+;;(defn- in? [coll x] (string/includes? x (string/join " " coll)))
+
+(defn- in? [descriptions description] (not (empty? (filter (fn [d] (string/includes? description d)) descriptions))))
 
 (defn group-records [records configuration]
   (defn records-for-descriptions [descriptions]
