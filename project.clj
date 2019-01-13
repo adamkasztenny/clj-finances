4369aafe531e69e5721389b959f741a7224512cd
Adam Kasztenny <adamkasztenny@gmail.com>
2019-01-13 09:54:44 -0500

    Fix up project.clj a little

--- project.clj
+++ project.clj
@@ -1,4 +1,8 @@
-(defproject leiningen.org "0.1.0"
+(defproject clj-finances "0.1.0"
   :description "Takes in a CSV file and groups expenses together"
+  :license {:name "GNU GPL v2"
+            :url "https://www.gnu.org/licenses/gpl-2.0.txt"}
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :main finances.core
 )
 
